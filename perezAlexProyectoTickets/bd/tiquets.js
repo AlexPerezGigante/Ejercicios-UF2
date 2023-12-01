991c0576afe75066a860b136ee4da86a5f836fc8
perezgigantealex <perezgigantealex@fpllefia.com>
2023-12-01 18:18:58 +0100

    Creado tiquets.js y añadidos los objetos #0

---
+++ perezAlexProyectoTickets/bd/tiquets.js
@@ -0,0 +1,90 @@
+export const tiquets = [
+ {
+    codigo: 123459,
+    fechaCreado: '18/04/2023',
+    fechaResuelto: '',
+    aula: 'T6',
+    grupo: 'DAW1',
+    ordenador: 'PC3',
+    descripcion: 'Error de impresora',
+    alumno: 'Ana Martínez',
+    estado: 0
+ },
+ {
+    codigo: 123460,
+    fechaCreado: '19/04/2023',
+    fechaResuelto: '',
+    aula: 'T8',
+    grupo: 'DAW2',
+    ordenador: 'PC4',
+    descripcion: 'Problema de acceso a archivos',
+    alumno: 'Pedro Gómez',
+    estado: 0
+ },
+ {
+    codigo: 123461,
+    fechaCreado: '20/04/2023',
+    fechaResuelto: '',
+    aula: 'T6',
+    grupo: 'DAW1',
+    ordenador: 'PC1',
+    descripcion: 'Aplicación se cierra inesperadamente',
+    alumno: 'Sofía Fernández',
+    estado: 0
+ },
+ {
+    codigo: 123462,
+    fechaCreado: '21/04/2023',
+    fechaResuelto: '',
+    aula: 'T7',
+    grupo: 'DAW2',
+    ordenador: 'PC2',
+    descripcion: 'Problema de conexión a la red',
+    alumno: 'Luis Torres',
+    estado: 0
+ },
+ {
+    codigo: 123463,
+    fechaCreado: '22/04/2023',
+    fechaResuelto: '',
+    aula: 'T8',
+    grupo: 'DAW1',
+    ordenador: 'PC3',
+    descripcion: 'Archivos corruptos',
+    alumno: 'Carolina Ramírez',
+    estado: 0
+ },
+ {
+    codigo: 123457,
+    fechaCreado: '16/04/2023',
+    fechaResuelto: '15/05/2023',
+    aula: 'T7',
+    grupo: 'DAW2',
+    ordenador: 'PC1',
+    descripcion: 'Problema de conexión a Internet',
+    alumno: 'Maria López',
+    estado: 1
+ },
+ {
+    codigo: 123458,
+    fechaCreado: '17/04/2023',
+    fechaResuelto: '15/05/2023',
+    aula: 'T8',
+    grupo: 'DAW1',
+    ordenador: 'PC2',
+    descripcion: 'Pantalla en blanco',
+    alumno: 'Juan Rodríguez',
+    estado: 1
+ },
+ {
+    codigo: 123459,
+    fechaCreado: '18/04/2023',
+    fechaResuelto: '15/05/2023',
+    aula: 'T8',
+    grupo: 'DAW1',
+    ordenador: 'PC3',
+    descripcion: 'Error de impresora',
+    alumno: 'Ana Martínez',
+    estado: 1
+ }
+]

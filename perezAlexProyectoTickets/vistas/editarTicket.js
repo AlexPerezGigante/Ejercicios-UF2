bd7036c7f156c60bce6524b9a4029148d435b41c
perezgigantealex <perezgigantealex@fpllefia.com>
2024-02-29 20:47:59 +0100

    .

--- perezAlexProyectoTickets/vistas/editarTicket.js
+++ perezAlexProyectoTickets/vistas/editarTicket.js
@@ -4,6 +4,35 @@ let html = ''
 
 html += `
 <main class="container mt-5">
+    <div class="d-flex">
+      <h1>Comentarios</h1><button class="btn btn-link ms-auto botonVolver" > < Volver</button>
+    </div>
+    
+    <h2 class="my-4">Código ticket: <span id="codigo">123456</span></h2>
+    <div class="">
+      <form action="" class="form card p-3 shadow">
+        <label for="comentario" class="form-label">Comentario: </label>
+        <textarea class="form-control inputComentario" col="3"></textarea>
+        <label for="fecha" class="form-label me-2 mt-3">Fecha: </label>
+        <div class="d-flex align-items-center">
+          <input type="datetime-local" class="form-control w-25 inputFecha">
+          <button class="btn btn-success ms-auto botonAgregar">Añadir comentario</button>
+        </div>
+      </form>
 
-</main>
+      <div class="mt-4" id="comentarios">
+        
+        
+      </div>
+    </div>
+    
+  </main>
 `
+
+export const editarTickets = {
+    template: html,
+
+    script:(ticket)=>{
+
+    }
+}
--- perezAlexProyectoTickets/vistas/panel.js
+++ perezAlexProyectoTickets/vistas/panel.js
@@ -1,5 +1,6 @@
 import { setTiquets ,getTiquets, lsSetDades, lsGetDades  } from "../bd/tiquets.js"
 import { comentarios } from "./comentarios.js"
+import { editarTickets } from "./editarTicket.js"
 let html=''
 
 html+= `
@@ -44,6 +45,19 @@ html+= `
 
         </tbody>
         </table>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Editar ticket</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        
+      </div>
+  </div>
+</div>
     </div>
     `
 export const panel = {
@@ -70,12 +84,12 @@ export const panel = {
                     <td>${element.descripcion}</td>
                     <td>${element.alumno}</td>
                     <td><button data-incidenciaid=${element.codigo} class="btn btn-success botonResolver" title="Resolver ticket">Resolver</button></td>
-                    <td><button data-incidenciaid=${element.codigo} class="btn btn-warning botonEditar" title="Añadir comentario"><i class="bi  bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                    <td><button data-incidenciaid=${element.codigo} class="btn btn-warning botonEditar"  title="Añadir comentario"><i data-incidenciaid=${element.codigo} class="bi  bi-pencil botonEditar" ></i>
                     </button>
                     </td>
-                    <td><button data-incidenciaid=${element.codigo} class="btn btn-info botonComentario" title="Ver comentarios"><i class="bi bi-chat-left-text"></i>
+                    <td><button data-incidenciaid=${element.codigo} class="btn btn-info botonComentario" title="Ver comentarios"><i data-incidenciaid=${element.codigo} class="bi bi-chat-left-text botonComentario"></i>
                     </button></td>
-                    <td><button data-incidenciaid=${element.codigo} class="btn btn-danger botonBorrar" title="Eliminar ticket"><i class="bi bi-trash3"></i>
+                    <td><button data-incidenciaid=${element.codigo} class="btn btn-danger botonBorrar" title="Eliminar ticket"><i data-incidenciaid=${element.codigo} class="bi bi-trash3 botonBorrar"></i>
                     </i>
                     </button></td>
                     </tr>
@@ -102,9 +116,9 @@ export const panel = {
                     <td>${element.descripcion}</td>
                     <td>${element.alumno}</td>
                     
-                    <td><button data-incidenciaid=${element.codigo} class="btn btn-info botonComentario" title="Ver comentarios"><i class="bi bi-chat-left-text"></i>
+                    <td><button data-incidenciaid=${element.codigo} class="btn btn-info botonComentario" title="Ver comentarios"><i data-incidenciaid=${element.codigo} class="bi bi-chat-left-text botonComentario"></i>
                     </button></td>
-                    <td><button data-incidenciaid=${element.codigo} class="btn btn-danger botonBorrar" title="Eliminar ticket"><i class="bi bi-trash3"></i>
+                    <td><button data-incidenciaid=${element.codigo} class="btn btn-danger botonBorrar" title="Eliminar ticket"><i data-incidenciaid=${element.codigo} class="bi bi-trash3 botonBorrar"></i>
                     </i>
                     </button></td>
                     </tr>
@@ -126,7 +140,6 @@ export const panel = {
                 // console.log('divTarea', divTarea)
                 // capturamos el id de la tarea en la que hemos hecho click
                 const idTarea = e.target.dataset.incidenciaid
-                 console.log('tareaId', idTarea)
         
                 const bdElementoBorrado = tiquets.filter((item)=>item.codigo != idTarea)
                 console.log(bdElementoBorrado)
@@ -137,14 +150,68 @@ export const panel = {
                 
             }
             if(e.target.classList.contains('botonEditar')){
-                console.log('editar tarea', e.target.classList)
-                const divTarea = e.target.closest('.tarea')
-                const idTarea = divTarea.dataset.tareaid
-                // const bdElementoEditado = datosBd.filter((item)=>item.id == idTarea)
-                document.querySelector('input').value = e.target.closest('.tarea').querySelector('.textoTarea').innerHTML
-                document.querySelector('.updateTarea').classList.remove('d-none')
-                document.querySelector('.addTarea').classList.add('d-none')
-                document.querySelector('input').dataset.tareaid = e.target.closest('.tarea').dataset.tareaid
+                const idTarea = e.target.dataset.incidenciaid
+
+                const elemento = tiquets.filter((item)=>item.codigo == idTarea)
+
+                let fecha = elemento[0].fechaCreado
+                fecha= fecha.split('/')
+                fecha= fecha[2] + '-' + fecha[1] + '-' + fecha[0] + 'T00:00:00'
+                
+                
+                const html = `
+                    <p>Código incidencia: <span id="codigo">${elemento[0].codigo}</span></p>
+                    <label for="fecha" class="form-label">Fecha:</label> 
+                    <input type="datetime-local" class="form-control fecha" value='${fecha}'>
+                    <div class="d-flex">
+                        <div>
+                            <label for="aula" class="form-label">Aula:</label> 
+                            <input type="text" class="form-control w-75  aula" value='${elemento[0].aula}'>
+                        </div>
+                        <div>
+                            <label for="grupo" class="form-label">Grupo:</label> 
+                            <input type="text" class="form-control w-75  grupo" value='${elemento[0].grupo}'>
+                        </div>
+                        <div>
+                            <label for="ordenador" class="form-label">Ordenador:</label> 
+                            <input type="text" class="form-control w-75 ordenador" value='${elemento[0].ordenador}'>
+                        </div>
+                    </div>
+                    
+            
+                    <label for="descripcion" class="form-label">Descripción:</label> 
+                    <input type="text" class="form-control descripcion" value='${elemento[0].descripcion}'>
+                    <label for="alumno" class="form-label">Alumno:</label> 
+                    <input type="text" class="form-control alumno" value='${elemento[0].alumno}'>
+                    
+                </div>
+                <div class="modal-footer">
+            
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary botonGuardar" >Guardar cambios</button>
+                </div>
+                `
+
+                document.querySelector('.modal-body').innerHTML = html
+
+                const modal = new bootstrap.Modal('#exampleModal');
+                modal.show();
+
+                const guardar = document.querySelector('.botonGuardar')
+                guardar.addEventListener('click', (e)=>{
+                    let date = document.querySelector('.fecha').value
+                    date = date + ''
+                    date = date.split('T')
+                    date = date[0].split('-')
+                    date = date[2] + '/' + date[1] + '/' + date[0]
+                    
+                    
+
+                    modal.hide()
+                    
+                })
+                // editarTickets.script(elemento[0])
+                
         
         
                 // pintarTareasPendientes(bdElementoBorrado)
